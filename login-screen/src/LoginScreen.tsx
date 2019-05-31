dd44d8308d24a79258288f9d1a5f69c117c9bf36
ChihYing <>
2019-05-31 00:18:24 -0400

    1. created login page 2. added email and password validation logics

---
+++ login-screen/src/LoginScreen.tsx
@@ -0,0 +1,154 @@
+import * as React from "react";
+import { 
+  Image, 
+  KeyboardAvoidingView, 
+  StyleSheet, 
+  View 
+} from "react-native";
+import Button from "./components/Button";
+import FormTextInput from "./components/FormTextInput";
+import imageLogo from "./assets/images/logo.png";
+import colors from "./config/colors";
+import strings from "./config/strings";
+
+interface State {
+    email: string;
+    password: string;
+    emailTouched: boolean;
+    passwordTouched: boolean;
+    emailValid: boolean;
+    passwordValid: boolean;
+}
+
+export default class LoginScreen extends React.Component<{}, State> {
+
+    passwordInputRef = React.createRef<FormTextInput>();
+
+    readonly state: State = {
+        email: "",
+        password: "",
+        emailTouched: false,
+        passwordTouched: false,
+        emailValid: false,
+        passwordValid: false
+    };
+
+    handleEmailChange = (email: string) => {
+      emailValid = email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+      if (!emailValid) {
+        this.setState({ email: email, emailValid: false });
+        console.log("Email is Not Correct");
+        return false;
+      } else {
+        console.log("Email is Correct");
+        this.setState({ email: email, emailValid: true });
+      }
+    };
+  
+    handlePasswordChange = (password: string) => {
+      passwordValid = password.length >= 6;
+      if (!passwordValid) {
+        this.setState({ password: password, passwordValid: false });
+        console.log("password is Not Correct");
+        return false;
+      } else {
+        console.log("password is Correct");
+        this.setState({ password: password, passwordValid: true });
+      }
+    };
+  
+    handleEmailSubmitPress = () => {
+      if (this.passwordInputRef.current) {
+        this.passwordInputRef.current.focus();
+      }
+    };
+  
+    handleEmailBlur = () => {
+      this.setState({ emailTouched: true });
+    };
+  
+    handlePasswordBlur = () => {
+      this.setState({ passwordTouched: true });
+    };
+  
+    handleLoginPress = () => {
+      console.log("Login button pressed");
+    };
+
+
+  render() {
+    const {
+      email,
+      password,
+      emailTouched,
+      passwordTouched,
+      emailValid,
+      passwordValid
+    } = this.state;
+
+    const emailError =
+      (!email && emailTouched) || (!emailValid && emailTouched)
+        ? strings.EMAIL_REQUIRED
+        : undefined;
+    const passwordError =
+      (!password || !passwordValid) && passwordTouched 
+        ? strings.PASSWORD_REQUIRED
+        : undefined;
+
+    return (
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior="padding">
+        <Image source={imageLogo} style={styles.logo} />
+          <View style={styles.form}>
+            <FormTextInput
+              value={this.state.email}
+              onChangeText={this.handleEmailChange}
+              onSubmitEditing={this.handleEmailSubmitPress}
+              placeholder={strings.EMAIL_PLACEHOLDER}
+              autoCorrect={false}
+              keyboardType="email-address"
+              returnKeyType="next"
+              onBlur={this.handleEmailBlur}
+              error={emailError}
+            />
+            <FormTextInput
+              ref={this.passwordInputRef}
+              value={this.state.password}
+              onChangeText={this.handlePasswordChange}
+              placeholder={strings.PASSWORD_PLACEHOLDER}
+              secureTextEntry={true}
+              returnKeyType="done"
+              onBlur={this.handlePasswordBlur}
+              error={passwordError} 
+            />
+            <Button 
+              label={strings.LOGIN} 
+              onPress={this.handleLoginPress}
+              disabled={!email || !password || !emailValid || !passwordValid}
+            />
+          </View>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.WHITE,
+      alignItems: "center",
+      justifyContent: "space-between"
+    },
+    logo: {
+      flex: 1,
+      width: "100%",
+      resizeMode: "contain",
+      alignSelf: "center"
+    },
+    form: {
+      flex: 1,
+      justifyContent: "center",
+      width: "80%"
+    }
+  });
---
+++ login-screen/src/config/strings.ts
@@ -0,0 +1,10 @@
+const strings = {
+    LOGIN: "Log In",
+    WELCOME_TO_LOGIN: "Welcome to the Irvine Company!",
+    EMAIL_PLACEHOLDER: "Email",
+    PASSWORD_PLACEHOLDER: "Password",
+    EMAIL_REQUIRED: "Email is not valid",
+    PASSWORD_REQUIRED: "Password is not valid"
+  };
+  
+  export default strings;
